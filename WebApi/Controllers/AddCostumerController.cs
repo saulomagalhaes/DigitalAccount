2c4edcfbca67c576a19a2c7b3b6cb909a08f18e0
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-02 16:25:58 -0300

    feat(customer): adiciona validação e personaliza erros do fluent validation

--- WebApi/Controllers/AddCostumerController.cs
+++ WebApi/Controllers/AddCostumerController.cs
@@ -1,7 +1,9 @@
 ﻿using Domain.Contracts.UseCases.AddCustomer;
 using Domain.Entities;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models.AddCustomer;
+using WebApi.Models.Error;
 
 namespace WebApi.Controllers
 {
@@ -10,15 +12,23 @@ namespace WebApi.Controllers
     public class AddCostumerController : ControllerBase
     {
         private readonly IAddCustomerUseCase _addCustomerUseCase;
-
-        public AddCostumerController(IAddCustomerUseCase addCustomerUseCase)
+        private readonly IValidator<AddCustomerInput> _addCustomerInputValidator;
+        public AddCostumerController(IAddCustomerUseCase addCustomerUseCase, IValidator<AddCustomerInput> addCustomerInputValidator)
         {
             _addCustomerUseCase = addCustomerUseCase;
+            _addCustomerInputValidator = addCustomerInputValidator;
         }
 
         [HttpPost]
         public IActionResult AddCustomer (AddCustomerInput input)
         {
+            var validateResult = _addCustomerInputValidator.Validate(input);
+
+            if(!validateResult.IsValid)
+            {
+                return BadRequest(validateResult.Errors.ToCustomValidationFailure());
+            }
+
             var customer = new Customer(input.Name, input.Email, input.Document);
 
             _addCustomerUseCase.AddCustomer(customer);
---
+++ WebApi/Models/Error/CustomValidationFailure.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi.Models.Error
+{
+    public class CustomValidationFailure
+    {
+        public CustomValidationFailure(string propetyName, string errorMessage)
+        {
+            PropertyName = propetyName;
+            ErrorMessage = errorMessage;
+
+        }
+        public string PropertyName { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
